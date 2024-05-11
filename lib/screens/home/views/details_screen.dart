6664eee5cf54b4fdaa2da80f62ea4c121133d915
SubhanAli94 <subhanali.softengg@gmail.com>
2024-05-11 19:39:54 +0400

    Details Screen UI implementation is complete.

--- lib/screens/home/views/details_screen.dart
+++ lib/screens/home/views/details_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pizza_app/components/macro.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
@@ -10,71 +13,140 @@ class DetailsScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: Column(
-          children: [
-            Material(
-              elevation: 4,
-              color: Colors.white,
-              shadowColor: Colors.grey.withOpacity(0.1),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0)),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width,
-                child: Image.asset('assets/1.png'),
+      body: Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+            children: [
+              Material(
+                elevation: 4,
+                color: Colors.white,
+                shadowColor: Colors.grey.withOpacity(0.1),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0)),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  child: Image.asset('assets/1.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 18,
-            ),
-            Material(
-              elevation: 4,
-              color: Colors.white,
-              shadowColor: Colors.grey.withOpacity(0.1),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0)),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 18.0, vertical: 18.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Text('Pizza Marvel: An excellent Pizza',
-                          style: TextStyle(
-                              fontSize: 18.0, fontWeight: FontWeight.bold)),
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
+              const SizedBox(
+                height: 18,
+              ),
+              Material(
+                elevation: 4,
+                color: Colors.white,
+                shadowColor: Colors.grey.withOpacity(0.1),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0)),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 18.0, vertical: 18.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            textAlign: TextAlign.end,
-                            '\$12',
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold),
+                          Expanded(
+                            flex: 2,
+                            child: Text('Pizza Marvel: An excellent Pizza',
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold)),
                           ),
-                          Text(
-                            '\$21',
-                            style: TextStyle(
-                                color: Colors.grey,
-                                decoration: TextDecoration.lineThrough,
-                                fontSize: 12.0,
-                                fontWeight: FontWeight.bold),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  textAlign: TextAlign.end,
+                                  '\$12',
+                                  style: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '\$21',
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      decoration: TextDecoration.lineThrough,
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          MyMacroWidget(
+                              title: 'Calories',
+                              value: 267,
+                              icon: FontAwesomeIcons.fire),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          MyMacroWidget(
+                              title: 'Protein',
+                              value: 36,
+                              icon: FontAwesomeIcons.dumbbell),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          MyMacroWidget(
+                              title: 'Fat',
+                              value: 21,
+                              icon: FontAwesomeIcons.oilWell),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          MyMacroWidget(
+                              title: 'Carbs',
+                              value: 38,
+                              icon: FontAwesomeIcons.breadSlice),
+                          SizedBox(
+                            width: 10,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: TextButton(
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                                elevation: 3.0,
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12))),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 25, vertical: 5),
+                              child: Text(
+                                'Buy Now',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            )),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
